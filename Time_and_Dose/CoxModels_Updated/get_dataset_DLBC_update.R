2169edba64dd7c5ea9d60f07cf35c69802108b92
Nicholas Panchy <nlpanchy@gmail.com>
2022-04-22 17:16:15 -0400

    Update get_dataset_DLBC_update.R

--- Time_and_Dose/CoxModels_Updated/get_dataset_DLBC_update.R
+++ Time_and_Dose/CoxModels_Updated/get_dataset_DLBC_update.R
@@ -4,7 +4,7 @@
 ### Setup Environemt ###
 
 # Folder
-setwd("E:/2021/Work_06_17_2021_TGFBTimeSeries/CoxModels_Updated/")
+setwd("<path_to_folder>/Time_and_Dose/CoxModels_Updated/")
 
 # Install Packages
 #if (!require("BiocManager", quietly = TRUE))
